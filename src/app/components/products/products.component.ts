85212b62bbfde2e1f1edf14b0c1e83beb5ae731b
Pierre Mortier <pierremortier1@gmail.com>
2022-12-06 14:22:14 +0100

    affichage get all products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product.model';
 import { ProductsService } from 'src/app/services/products.service';
+import { catchError, Observable, startWith, map, of } from 'rxjs';
+import { AppDataState, DataStateEnum } from 'src/app/state/product.state';
 
 @Component({
   selector: 'app-products',
@@ -7,10 +10,22 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products$:Observable<AppDataState<Product[]>> | null = null;
+  readonly DataStateEnum=DataStateEnum;
 
   constructor(private productsService:ProductsService) { }
 
   ngOnInit(): void {
+  }
+
+  onGetAllProducts(){
+    this.products$ = this.productsService.getAllProducts().pipe(
+      map(data=>{
+        return ({dataState:DataStateEnum.LOADED,data:data})
+      }),
+      startWith({dataState:DataStateEnum.LOADING}),
+      catchError(err=>of({dataState:DataStateEnum.ERROR,errorMessage:err.message}))
+    );
   }
 
 }
